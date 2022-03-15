991663232e79dcac58a89d2c7b5432da474a93c8
Nasir Usman <nasirusman92@gmail.com>
2022-03-15 22:24:51 +0800

    Mengubah title utama

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'Laravel Docs',
   tagline: 'Dinosaurs are cool',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
@@ -43,7 +43,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'Laravel Docs',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
